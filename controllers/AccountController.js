315e84c345138a237845e2434ce7b4e311793ca8
HadasMu <hadas0125@gmail.com>
2022-01-03 17:02:12 +0200

    cookies

---
+++ controllers/AccountController.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const router = express.Router();
+const Cookies = require('cookies');
+const keys = ['keyboard cat'];
+const db = require('../models');
+
+exports.getLogin = (req, res, next) => {
+  res.render('login',{ errorMessage:'' });
+};
+
+exports.PostLogin = (req, res, next) => {
+    db.Account.findOne(req.body.email.toLowerCase())
+      .then(account => {
+        if (account.password === req.body.password) {
+          res.render('nasa', {title: 'Express'});
+        }
+        else
+        {
+          res.render("login", {errorMessage: ' The email address or password you entered isn\'t match to an account.\n'});
+        }
+      })
+      .catch((err) => {
+        return res.status(400).send(err)
+      })
+};
+
+exports.getRegister = (req, res, next) => {
+  res.render('register', { errorMessage:'' });
+};
+
+exports.postRegister = (req, res, next) => {
+  let email = req.body.email.toLowerCase();
+  res.render('register',  () => {
+    const regexEmail = "/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/";
+    if(email === '' || req.body.first_name === '' || req.body.family_name === ''
+        || email.match(regexEmail))
+    {
+      res.status(404).send(`not valid request`);
+    }
+    else{
+      db.Account.findById(req.body.email.toLowerCase())
+          .then(account => {
+            if(account)
+              res.render('register', { errorMessage:'email already register' });
+            else
+            {
+              const cookies = new Cookies(req, res, { keys: keys });
+              cookies.set('passRegister', true , { signed: false});
+              cookies.set('email', req.body.email, { signed: false});
+              cookies.set('first_name', req.body.first_name, { signed: false});
+              cookies.set('family_name', req.body.family_name, { signed: false});
+              cookies.set('startClock', new Date().toISOString(), { signed: false, maxAge: 10*1000  });
+              return res.redirect("password");
+            }
+          })
+          .catch((err) => {
+            return res.status(400).send(err)
+          })
+    }
+  });
+};
+
+exports.getPassword = (req, res, next) => {
+  if(req.cookies["passRegister"] !== "true")
+    return res.redirect("register");
+  res.render('password', { data:req.body , title: 'Express' });
+};
+
+exports.postPassword = (req, res, next) => {
+  const cookies = new Cookies(req, res, { keys: keys });
+  cookies.set('passRegister', false , { signed: false});
+  if (req.cookies["startClock"] == null)
+  {
+    return res.redirect("/register");
+  }
+  res.render('password',  () => {
+    if(req.body.confirm_pass === '' || req.body.password === '' || req.body.password !== req.body.confirm_pass)
+    {
+      res.status(404).send(`not valid request`);
+    }
+    else
+    {
+      db.Account.create({
+        first_name: req.cookies["first_name"] ,
+        last_name: req.cookies["family_name"],
+        mail: req.cookies["email"],
+        pass:req.body.password
+    })
+      return res.redirect("/");
+    }
+  });
+};
+
+
--- routes/index.js
+++ routes/index.js
@@ -1,79 +1,28 @@
+const path = require('path');
+
 const express = require('express');
-const User = require('../models/User');
+
+const AccountController = require('../controllers/AccountController');
+
 const router = express.Router();
-const Cookies = require('cookies');
-const keys = ['keyboard cat'];
 
+// / ROUTE => GET
+router.get('/', AccountController.getLogin);
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('login',{ errorMessage:'' });
-});
+//  / ROUTE  => POST
+router.post('/', AccountController.PostLogin);
 
-router.get('/register', function(req, res, next) {
-  res.render('register', { errorMessage:'' });
-});
+// /register ROUTE => GET
+router.get('/register', AccountController.getRegister) ;
 
-router.post('/register', function(req, res, next) {
-  let email = req.body.email.toLowerCase();
-  res.render('register',  () => {
-    const regexEmail = "/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/";
-    if(email === '' || req.body.first_name === '' || req.body.family_name === ''
-        || email.match(regexEmail))
-    {
-      res.status(404).send(`not valid request`);
-    }
-    else if(User.fetchAll().find(user => user.email === email))
-    {
-      res.render('register', { errorMessage:'email already register' });
-    }
-    else
-    {
-      const cookies = new Cookies(req, res, { keys: keys });
-      cookies.set('passRegister', true , { signed: false});
-      cookies.set('email', req.body.email, { signed: false});
-      cookies.set('first_name', req.body.first_name, { signed: false});
-      cookies.set('family_name', req.body.family_name, { signed: false});
-      cookies.set('startClock', new Date().toISOString(), { signed: false, maxAge: 10*1000  });
-      return res.redirect("password");
-    }
-  });
-});
+// /register ROUTE => POST
+router.post('/register', AccountController.postRegister);
 
-router.get('/password', function(req, res, next) {
-  if(req.cookies["passRegister"] !== "true")
-    return res.redirect("register");
-  res.render('password', { data:req.body , title: 'Express' });
-});
+// /password ROUTE => GET
+router.get('/password', AccountController.getPassword) ;
 
-router.post('/password', function(req, res, next) {
-  const cookies = new Cookies(req, res, { keys: keys });
-  cookies.set('passRegister', false , { signed: false});
-  if (req.cookies["startClock"] == null)
-  {
-    return res.redirect("/register");
-  }
-  res.render('password',  () => {
-    if(req.body.confirm_pass === '' || req.body.password === '' || req.body.password !== req.body.confirm_pass)
-    {
-      res.status(404).send(`not valid request`);
-    }
-    else
-    {
-      let newUser = new User(req.cookies["email"],req.cookies["first_name"],req.cookies["family_name"] , req.body.password);
-      newUser.save();
-      return res.redirect("/");
-    }
-  });
-});
-
-router.post('/', function(req, res, next) {
-  let checkUser = User.fetchAll().find(user => user.email === req.body.email.toLowerCase());
-  if(checkUser && checkUser.password === req.body.password) {
-    res.render('nasa', {title: 'Express'});
-  }
-  res.render("login", { errorMessage:' The email address or password you entered isn\'t match to an account.\n' });
-});
+// /password ROUTE => POST
+router.post('/password', AccountController.postPassword);
 
 router.get('/nasa', function(req, res, next) {
   res.render('nasa', { title: 'Express' });
